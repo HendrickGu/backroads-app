35a0634c304f237c4a24840009bd4cc4d0d5c78c
Jiaheng Gu <a1441573231@outlook.com>
2024-02-14 14:56:47 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Service from "./components/Service";
+import Tours from "./components/Tours";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Service />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,33 @@
+export const pageLinks = [
+  { id: 1, href: "#home", text: "home" },
+  { id: 2, href: "#about", text: "about" },
+  { id: 3, href: "#services", text: "services" },
+  { id: 4, href: "#tours", text: "tour" },
+];
+
+export const socialLinks = [
+  { id: 1, href: "https://www.twitter.com", icon: "fab fa-facebook" },
+  { id: 1, href: "https://www.twitter.com", icon: "fab fa-squarespace" },
+  { id: 1, href: "https://www.twitter.com", icon: "fab fa-twitter" },
+];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+  },
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "endless hiking",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia1.",
+  },
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "amazing comfort",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia2.",
+  },
+];
